68843a22262dc2f4948737b07b6cfc85369f9dc3
Fmaldonado6 <f_maldonado6@hotmail.com>
2020-04-23 16:31:16 -0500

    Loading fix

--- js/index.js
+++ js/index.js
@@ -2,14 +2,17 @@ $(document).ready(function () {
   $('.sidenav').sidenav();
   $('.slider').slider();
   $('.collapsible').collapsible();
-  $(".loading").addClass("loadedPage")
-  setTimeout(()=>{
-    $(".loading").remove()
-  },1000)
+
   let parameter = window.location.href.split("#");
   let currentPage = parameter.length > 1 ? parameter.pop() : "home"
   changePage(currentPage + "Btn")
 });
+$(window).on('load',(()=>{
+  $(".loading").addClass("loadedPage")
+  setTimeout(()=>{
+    $(".loading").remove()
+  },1000)
+}))
 
 function changePage(current){
   $("li").removeClass("selectedTopic");
