3bdb96ec4782b000dfe091ff2b7aeaf0ce52bfd7
Fmaldonado6 <f_maldonado6@hotmail.com>
2020-04-23 16:27:16 -0500

    Loading state

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
   $('.sidenav').sidenav();
   $('.slider').slider();
   $('.collapsible').collapsible();
+  $(".loading").addClass("loadedPage")
+  setTimeout(()=>{
+    $(".loading").remove()
+  },1000)
   let parameter = window.location.href.split("#");
   let currentPage = parameter.length > 1 ? parameter.pop() : "home"
   changePage(currentPage + "Btn")
